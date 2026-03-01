2ae790c5f3d3b0a0a8082c7b9a35590081348a31
mirik2022 <97746530+mirik2022@users.noreply.github.com>
2026-03-01 15:14:49 +0300

    Update Form1.Designer.cs

--- Lab1/CryptoLab1/Form1.Designer.cs
+++ Lab1/CryptoLab1/Form1.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
+
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +16,6 @@
 
         #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             comboBoxAlgorithm = new ComboBox();
@@ -340,4 +330,5 @@
         private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
     }
+
 }
