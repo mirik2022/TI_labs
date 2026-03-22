30b767cb70a428658989c599bf72868f215690f6
Miroslav <97746530+mirik2022@users.noreply.github.com>
2026-03-22 13:11:07 +0300

    Update

--- Lab2/Файлы проекта/LFSR_StreamCipher/Form1.Designer.cs
+++ Lab2/Файлы проекта/LFSR_StreamCipher/Form1.Designer.cs
@@ -2,15 +2,10 @@
 {
     partial class Form1
     {
-        /// <summary>
         /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
         /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +17,6 @@
 
         #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             label2 = new Label();
